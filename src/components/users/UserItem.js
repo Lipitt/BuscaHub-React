533931d5252d56d2806c6737e2bb56fc81d7b198
Lipitt <victor.david.lip@gmail.com>
2021-01-20 17:09:10 -0300

    componente usuarios

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
+  
     render() {
+        const {login, avatar_url , html_url} = this.props.user;
         return (
-            <div>UserItem</div>
+            <div className="card text-center">
+                <img 
+                src={avatar_url} 
+                alt="" 
+                className="round-img" 
+                style={{width: "60px"}}
+                />
+                <h3>{login}</h3>
+                <div>
+                    <a className="btn btn-dark btn-sm my-1" href={html_url}>Ver Perfil</a>
+                </div>
+                </div>
         )
     }
 }
