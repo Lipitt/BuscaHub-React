643d5a6d9b61921ca47de80d8fa86c0f01b0bd17
Lipitt <victor.david.lip@gmail.com>
2021-01-21 20:55:43 -0300

    agregada barra de busqueda

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
 
 export class Search extends Component {
     state = {
         text: "",
         
     }
-    onChange = (e) => this.setState({[e.target.name]: e.target.value});
+    static PropTypes = {
+        searchUsers: PropTypes.func.isRequired,
+    }
+    onChange = e => this.setState({[e.target.name]: e.target.value});
     onSubmit = (e) => {
+        e.preventDefault();
+        this.props.searchUsers(this.state.text);
+        this.setState({text: ""});
         
     }
     
     render() {
         return (
             <div>
-                <form className="form">
+                <form onSubmit = {this.onSubmit} className="form">
                     <input 
                     type="text" 
                     name="text" 
                     placeholder="Busca Usuarios..." 
                     value={this.state.text} 
                     onChange={this.onChange}
-                    onSubmit={this.onSubmit}
                     />
                     <input type="submit" value="Buscar" className="btn btn-dark btn-block"/>
                 </form>
