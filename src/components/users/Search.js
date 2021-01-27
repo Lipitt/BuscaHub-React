ffde7c4fc614eea1bddd493eddfb3def591b9c55
Lipitt <victor.david.lip@gmail.com>
2021-01-27 17:35:14 -0300

    agregado boton limpiar

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,8 +8,10 @@ export class Search extends Component {
         text: "",
         
     }
-    static PropTypes = {
+    static propTypes = {
         searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired
     }
     onChange = e => this.setState({[e.target.name]: e.target.value});
     onSubmit = (e) => {
@@ -18,23 +20,35 @@ export class Search extends Component {
         this.setState({text: ""});
         
     }
+   
+ 
     
     render() {
+        const {showClear, clearUsers} = this.props;
         return (
             <div>
                 <form onSubmit = {this.onSubmit} className="form">
                     <input 
                     type="text" 
                     name="text" 
+                    id="pepe"
                     placeholder="Busca Usuarios..." 
                     value={this.state.text} 
                     onChange={this.onChange}
                     />
                     <input type="submit" value="Buscar" className="btn btn-dark btn-block"/>
                 </form>
+                {showClear && (
+                    <button 
+                        className="btn btn-light btn-block" 
+                        onClick={clearUsers
+                        }>Limpiar
+                    </button>)}
+                
             </div>
         )
     }
+    
 }
 
 export default Search
