74451b3dbf77bfdde576267160da78f2e0fe8afe
Lipitt <victor.david.lip@gmail.com>
2021-02-18 09:39:01 -0300

    agregados componentes home y acerca de

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Pagina no encontrada</h1>
+      <p className="lead">La pagina que estas buscando no existe</p>
+    </div>
+  );
+};
+
+export default NotFound;
